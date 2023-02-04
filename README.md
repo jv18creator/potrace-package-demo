1d9cdb99bc82ae0ca41cdfc0801fa5d4bd44e8c4
jv18creator <jviramgama5@gmail.com>
2023-02-04 14:26:02 +0530

    Init

---
+++ README.md
@@ -0,0 +1,9 @@
+#Install packages
+npm i
+
+#Start Server
+npm start
+
+Official website: https://potrace.sourceforge.net/#description
+NPM Package: https://www.npmjs.com/package/potrace
+Javascript Docs: https://github.com/tooolbox/node-potrace
---
+++ index.js
@@ -0,0 +1,26 @@
+const http = require("http");
+const potrace = require("potrace"),
+  fs = require("fs");
+const { randomIntFromInterval } = require("./utils");
+
+// You'll find the SVG's in converted folder
+potrace.trace(
+  `./images/${randomIntFromInterval(1, 5)}.jpg`,
+  //   { steps: [40, 85, 135, 180], color: "#ccc", background: "#0000FF" }, PARAMS
+  function (err, svg) {
+    if (err) throw err;
+    fs.writeFileSync(
+      `./converted/output_${randomIntFromInterval(5, 1000)}.svg`,
+      svg
+    );
+  }
+);
+
+http
+  .createServer(function (req, res) {
+    res.write(
+      "<h1>Hey, this is a demo of how to get started with potrace!!!</h1>"
+    );
+    res.end();
+  })
+  .listen(8080);
